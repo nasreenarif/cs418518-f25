04c6877979ce089ab97a4a9a7912a06d74f5223b
Nasreen Arif <nasreen_arif@live.com>
2025-09-09 13:18:59 -0400

    HTML,CSS, JS overview

---
+++ HTML&CSS/script.js
@@ -0,0 +1,59 @@
+//Variables
+// let userMessage = "Hello World";
+
+// console.log(userMessage);
+
+//  let user='Nasreen';
+//  let age=34, message='Hello';
+
+//  console.log(message + ' ' + user);
+//  console.log('She is ' +age +' years old');
+
+//***************Constant*********
+// age=33;
+
+// console.log(typeof(age));
+
+// const constAge=34;
+// constAge=33;
+
+// console.log(constAge);
+
+//*********JS is case sensitive */
+
+// let user='Nasreen';
+// let User='Arif';
+
+// console.log(user);
+// console.log(User);
+
+// *********Object in JavaScript************
+// const user = {
+//   name: "Nasreen",
+//   age: 33,
+//   gender:'F'
+// };
+
+// console.log(user.name);
+// console.log(user.age);
+// console.log(user.gender);
+
+// **************Array*********
+
+// const hobbies =["Sports","Cooking","Reading"];
+// console.log(hobbies[0]);
+
+// ******Import Export********
+
+export let aiken = "asasasasasas";
+import { apiKey1 } from "./util.js";
+console.log(apiKey1);
+console.log(aiken);
+
+
+//**************Ternary Operator */
+
+
+// let age=30;
+// let checkage=age>=33 ? true :false;
+// console.log(checkage)
